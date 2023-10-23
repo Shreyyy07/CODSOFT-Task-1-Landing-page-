0aad23ff1f9dd6977b722b16bee1b93814096577
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2023-10-23 14:54:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// const showNav = () => {
+//     console.log('lorem');
+//     let h = document.querySelectorAll('.hide');
+//     h.forEach(element => {
+//         if (element.style.display === 'block') {
+//             element.style.display = 'none'; // Hide if currently shown
+//         } else {
+//             element.style.display = 'block'; // Show if currently hidden
+//         }
+//     });
+// }
